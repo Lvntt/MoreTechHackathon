336b9211a60f75c430ae5dce8260e8f672ea4c15
Lantt <daymatt@mail.ru>
2023-10-13 23:30:55 +0700

    Update compileSdk version 33 -> 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.phrogs.moretechhackathon"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.phrogs.moretechhackathon"
