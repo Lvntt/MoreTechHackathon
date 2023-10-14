c1bde370cb1362e2e6f4768756413420b461afcc
Lantt <daymatt@mail.ru>
2023-10-14 19:30:10 +0700

    Add fonts, navigation and text styles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("io.insert-koin:koin-core:3.3.2")
     implementation("io.insert-koin:koin-android:3.3.2")
     implementation("io.insert-koin:koin-androidx-compose:3.4.1")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/ru/phrogs/moretechhackathon/presentation/ui/theme/TextStyle.kt
@@ -0,0 +1,71 @@
+package ru.phrogs.moretechhackathon.presentation.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Title = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 21.sp,
+    fontWeight = FontWeight.Medium
+)
+
+val Headline = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 20.sp,
+    fontWeight = FontWeight.Medium
+)
+
+val Subtitle = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 16.sp,
+    fontWeight = FontWeight.Medium
+)
+
+val Text = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 16.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val LabelAccentSemiBold = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 13.sp,
+    fontWeight = FontWeight.SemiBold
+)
+
+val LabelAccent = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 13.sp,
+    fontWeight = FontWeight.Medium
+)
+
+val LabelRegular = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 13.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val OnButtonTextStyle = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 20.sp,
+    fontWeight = FontWeight.Medium
+)
+
+val OnSmallButtonTextStyle = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 14.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val TopBarTextStyle = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 22.sp,
+    fontWeight = FontWeight.Medium
+)
+
+val AdditionalText = TextStyle(
+    fontFamily = VTBGroupUI,
+    fontSize = 11.sp,
+    fontWeight = FontWeight.Normal
+)
