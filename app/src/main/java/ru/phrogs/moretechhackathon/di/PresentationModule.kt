f42bfcd2174fb333c64d127752de52025a28130d
Suslanium <zaharov22m@gmail.com>
2023-10-15 11:31:49 +0700

    OnboardingScreen

--- app/src/main/java/ru/phrogs/moretechhackathon/di/PresentationModule.kt
+++ app/src/main/java/ru/phrogs/moretechhackathon/di/PresentationModule.kt
@@ -3,11 +3,16 @@ package ru.phrogs.moretechhackathon.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.phrogs.moretechhackathon.presentation.viewmodel.MapViewModel
+import ru.phrogs.moretechhackathon.presentation.viewmodel.OnboardingViewModel
 
 fun providePresentationModule() = module {
 
     viewModel {
         MapViewModel(get(), get())
+    }
+
+    viewModel {
+        OnboardingViewModel()
     }
 
 }
---
+++ app/src/main/java/ru/phrogs/moretechhackathon/domain/entity/DisabilityType.kt
@@ -0,0 +1,7 @@
+package ru.phrogs.moretechhackathon.domain.entity
+
+enum class DisabilityType {
+    VISION,
+    MOVEMENT,
+    NONE
+}
